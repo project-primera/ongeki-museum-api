e89bcdb99ebf36e47173939365d571d9b620e1cc
Slime-hatena <git@aki-memo.net>
2025-03-09 01:43:41 +0900

    refactor: インターフェースを切り分け

--- OngekiMuseumApi/OngekiMuseumApi/Services/OfficialMusicService.cs
+++ OngekiMuseumApi/OngekiMuseumApi/Services/OfficialMusicService.cs
@@ -150,18 +150,6 @@ namespace OngekiMuseumApi.Services
     }
 
     /// <summary>
-    /// ONGEKI公式楽曲データの取得と保存を行うサービスのインターフェース
-    /// </summary>
-    public interface IOfficialMusicService
-    {
-        /// <summary>
-        /// 公式サイトから楽曲データを取得し、データベースに保存する
-        /// </summary>
-        /// <returns>非同期タスク</returns>
-        Task FetchAndSaveOfficialMusicAsync();
-    }
-
-    /// <summary>
     /// JSONデータをデシリアライズするためのクラス
     /// </summary>
     // ReSharper disable once ClassNeverInstantiated.Global
